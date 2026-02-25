644fd041087cbb6bee44ed895a615b9d09f735cc
Bianca Vieira <b.lorranyvieira@gmail.com>
2026-02-25 14:42:02 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,7 @@
+var1 = 10
+Var1 = 20
+
+print(var1)
+print(Var1)
+
+#Foi expresso dois valores diferentes mas com o mesmo nome. Mas a difereça fica com uma diferença entre o início ter letra maiuscula e outra minuscula
