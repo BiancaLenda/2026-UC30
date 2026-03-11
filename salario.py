8fbb72fe5802fffc41c083de472081fdd490c75a
Bianca Vieira <b.lorranyvieira@gmail.com>
2026-03-11 11:23:00 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,18 @@
+salario_base = 3500.00  
+bonus = 800.00           
+desconto = 250.00        
+
+salario_bruto = salario_base + bonus
+salario_liquido = salario_bruto - desconto
+
+print("Salário base:", salario_base)
+print("Bônus:", bonus)
+print("Desconto:", desconto)
+print("Salário bruto:", salario_bruto)
+print("Salário líquido:", salario_liquido)
+
+print("tipo de dado de salario_base:", type(salario_base))
+print("tipo de dado de bonus:", type(bonus))
+print("tipo de dado de desconto:", type(desconto))
+print("tipo de dado de salario_bruto:", type(salario_bruto))
+print("tipo de dado de salario_liquido:", type(salario_liquido))
